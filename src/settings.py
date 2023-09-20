aaff6c0cea6d54f391921c22ccca2aa5cd3604b1
Jordan Evans <jordan.evans@yard.email>
2023-09-20 13:07:05 +0100

    - Add settings file which holds global constants

---
+++ src/settings.py
@@ -0,0 +1,17 @@
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+# Chess board dimensions
+
+ROWS = 8
+COLS = 8
+SQUARE_SIZE = SCREEN_WIDTH // COLS
+TITLE = "Chess"
+
+# Board Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+DARK_GREEN = (119, 149, 86)
+LIGHT_GREEN = (163, 154, 88)
